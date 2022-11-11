42073a7e60d22a55a8126052ef66373bfa99ed6b
Moisés Sánchez <moy.sav21@hotmail.com>
2022-11-10 22:07:52 -0600

    Initial Commit

---
+++ Ejercicio1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio1
+//
+//  Created by DISMOV on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
